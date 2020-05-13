72cfc7fb518f2ef443c22a110645f3239f11a407
hyl2099 <hyl2099@gmail.com>
2020-05-13 23:16:47 +0200

    add PictureController

--- src/main/java/com/smartroom/springServer/api_rest_controllers/PictureResource.java
+++ src/main/java/com/smartroom/springServer/api_rest_controllers/PictureResource.java
@@ -1,4 +1,40 @@
 package com.smartroom.springServer.api_rest_controllers;
 
+import com.smartroom.springServer.business_controllers.PictureController;
+import com.smartroom.springServer.documents.Picture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+// 声明为Rest控制器（支持前后台分离)
+@RestController
 public class PictureResource {
+    public static final String PICTURES = "/pictures";
+    public static final String ID = "/{id}";
+
+    private PictureController pictureController;
+
+    @Autowired
+    public PictureResource(PictureController pictureController) {
+        this.pictureController = pictureController;
+    }
+
+    // 设置路由
+    @RequestMapping("/pictures/save")
+    // 使用@RequestBody注解，将请求的`json`数据，直接加载至teacher对象
+    public Picture savePicture(@RequestBody Picture picture) {
+        // 打印加载的数据
+        System.out.println(picture);
+
+        // 调用保存操作
+        return pictureController.savePicture(picture);
+    }
+
+    @GetMapping
+    public Flux<Picture> readAll(){
+        return pictureController.readAll();
+    }
 }
--- src/main/java/com/smartroom/springServer/business_controllers/PictureController.java
+++ src/main/java/com/smartroom/springServer/business_controllers/PictureController.java
@@ -1,7 +1,27 @@
 package com.smartroom.springServer.business_controllers;
 
+import com.smartroom.springServer.documents.Picture;
+import com.smartroom.springServer.repositories.PictureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 
+@Controller
 public class PictureController {
+    PictureRepository pictureRepository;
+
+    //自动装入pictureRepository
+    @Autowired
+    public PictureController(PictureRepository pictureRepository) {
+        this.pictureRepository = pictureRepository;
+    }
+
+    public Flux<Picture> readAll() {
+        return (Flux<Picture>) this.pictureRepository.findAll();
+    }
+
+    public Picture savePicture(Picture picture){
+        return this.pictureRepository.save(picture);
+    }
 
 }
