80d5b0d5c59b41234e99e9aaf62a49a025ada498
hyl2099 <hyl2099@gmail.com>
2020-05-30 16:36:36 +0200

    fix login problem.

--- src/main/java/com/smartroom/springServer/WebMvcConfig.java
+++ src/main/java/com/smartroom/springServer/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.smartroom.springServer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,4 +14,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").maxAge(3600);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:D:\\UPM_MASTER_MIW\\mater_MIW_UPM\\10-TFM\\SmartRoom-Pictures\\");
+    }
 }
