dd8b543b3baa27f863e16d8c588f317664272272
hyl2099 <hyl2099@gmail.com>
2020-06-07 12:15:31 +0200

    add addPhoto post API for android client

--- src/main/java/com/smartroom/springServer/api_rest_controllers/PictureResource.java
+++ src/main/java/com/smartroom/springServer/api_rest_controllers/PictureResource.java
@@ -1,20 +1,21 @@
 package com.smartroom.springServer.api_rest_controllers;
 
 import com.smartroom.springServer.business_controllers.PictureController;
+import com.smartroom.springServer.business_controllers.PictureUploadController;
 import com.smartroom.springServer.documents.Picture;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 import javax.servlet.ServletContext;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.File;
 
 // 声明为Rest控制器（支持前后台分离)
 @RestController
@@ -25,12 +26,14 @@ public class PictureResource {
     private String IMAGEPATH = "D:\\UPM_MASTER_MIW\\mater_MIW_UPM\\10-TFM\\SmartRoom-Pictures";
     private String imagePath;
     private PictureController pictureController;
+    private PictureUploadController pictureUploadController;
 
     @Autowired
     ServletContext context;
     @Autowired
-    public PictureResource(PictureController pictureController) {
+    public PictureResource(PictureController pictureController,PictureUploadController pictureUploadController) {
         this.pictureController = pictureController;
+        this.pictureUploadController = pictureUploadController;
     }
 
     // 设置路由
@@ -206,5 +209,48 @@ public class PictureResource {
         }
         return new ResponseEntity<List<Picture>>(pictures, HttpStatus.OK);
     }
+    //笔记：@PathVariable 和 @RequestParam差别
+    //@PathVariable is used to tell Spring that part of the URI path is a value you want passed to your method.
+    //If you want @PathVariable, you need to specify placeholders in the @RequestMapping entry to tell Spring where the path variables fit in the URI.
+    // For example, if you want to extract a path variable called contentId, you would use:
+    //@RequestMapping(value = "/whatever/{contentId}", method = RequestMethod.POST)
+    //If you want form data, use @RequestParam instead of @PathVariable.
+    @PostMapping("/pictures/addPhoto")
+    @ResponseBody
+    public Picture AddPhotoFromAndroid(@RequestParam("file") MultipartFile file,
+                                       @RequestParam String owner,
+                                       @RequestParam String remark
+                                       ) throws IOException {
+        if(file.isEmpty()){
+            System.out.println("file empty!");
+        }
+        //使用时间给上传的文件命名，这种方式没有用uuid命名好，因为同一时间有可能会上传多个文件
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSS");
+        String res = sdf.format(new Date());
+        String originaFilename = file.getOriginalFilename();
+        //获取文件的后缀名
+        String newFileName = res+originaFilename.substring(originaFilename.lastIndexOf("."));
+        // 跟目录设置是在SpringServerApplication 中的MultipartConfigElement中设置的。
+        String rootPath = "/pictures/";
+        File newFile = new File(rootPath+newFileName);
+        System.out.println(rootPath+newFileName);
+        //定义向数据库中存取的文件路径
+        String src=rootPath+newFileName;
+        if(!newFile.getParentFile().exists()){
+            newFile.getParentFile().mkdirs();
+        }else{
+            System.out.println(newFile.getParentFile());
+        }
+        if(!newFile.exists()){
+            //使用transferTo()方法将文件存到所在服务器上
+            file.transferTo(newFile);
+        }
+        Picture picture = new Picture();
+        picture.setOwner(owner);
+        picture.setUploadTime(new Date());
+        picture.setRemark(remark);
+        return pictureUploadController.add(src,picture);
+    }
+
 
 }
